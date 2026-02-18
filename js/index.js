76edc059045acee35b74bcaac638a55cb9059915
Mariia Tsenkush <239046230+MTsenkush@users.noreply.github.com>
2026-02-17 23:04:26 -0600

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -41,9 +41,8 @@ skillsList.appendChild(newSkill);
 
 //If we dont have <footer> in HTML: const footer = document.createElement("footer");
 //document.body.appendChild(footer);
+
 // const thisYear=new Date().getFullYear();
-
-
 const today = new Date();
 const thisYear = today.getFullYear();
 
@@ -54,3 +53,42 @@ const copyright = document.createElement("p");
 copyright.innerText = `© Mariia Tsenkush ${thisYear}`;
 
 footer.appendChild(copyright);
+
+
+//MESSAGES
+
+//const messageForm = document.querySelector('form[name="leave_message"]');
+const messageForm = document.getElementsByName("leave_message")[0];
+
+
+messageForm.addEventListener("submit",function(event){
+    
+    event.preventDefault();
+    
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    console.log(usersName, usersEmail, usersMessage);
+
+    const messageSection = document.getElementById("messages");
+    const messageList = messageSection.querySelector("ul");
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a>
+        <span>${usersMessage}</span>`;
+
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+
+    removeButton.addEventListener("click", function(){
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+});
